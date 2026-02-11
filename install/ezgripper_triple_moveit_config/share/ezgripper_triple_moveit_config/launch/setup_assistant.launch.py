99de28caffd095c4ed2111b9448b52a71cf90f46
vickkrish <vick.krish@hotmail.com>
2026-02-10 20:59:57 -0500

    remove build dirs

--- install/ezgripper_triple_moveit_config/share/ezgripper_triple_moveit_config/launch/setup_assistant.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_setup_assistant_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ezgripper_triple_mount_instantiation", package_name="ezgripper_triple_moveit_config").to_moveit_configs()
-    return generate_setup_assistant_launch(moveit_config)
